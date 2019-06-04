8dc744f18d3661b4a3df70535de22e9a0218470c
Kanika Batra <kanikabatra@gmail.com>
2019-06-04 14:15:46 -0500

    Saved changes

---
+++ python/python/street_exercise.py
@@ -0,0 +1,12 @@
+address_info = {
+"name": "Kanika",
+"street": "123 Definitely not",
+"city": "XYZ",
+"state": "TX",
+"zip": "0000,"
+}
+
+print (address_info['name'])
+print (address_info['street']),
+ address_info['state'],
+address_info['zip]'])
