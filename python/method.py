1bea4d5e40075f95ddf5177cc8629034a161d224
Kanika Batra <kanikabatra@gmail.com>
2019-06-05 10:29:39 -0500

    Text metadata file.

--- python/method.py
+++ python/method.py
@@ -5,3 +5,12 @@ texts = ["x", "y", "z", "l"]
 
 for text in texts:
 	say_this(text)
+
+
+def method_one():
+    print("look I'm a method.")
+
+def method_two(name):
+    print("about to call our first method for" + name)
+    method_one()
+    method_two("Brandon")
